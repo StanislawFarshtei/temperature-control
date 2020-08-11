456bbf399554446ad4fbd8e55666ef144a3b961b
StanislawFarshtei <64971911+StanislawFarshtei@users.noreply.github.com>
2020-08-11 11:09:55 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  let [tempColor, setTempColor] = useState('cold');
+  const [value, setValue] = useState(10);
+  const[isRunning, setIsRunning] = useState(false);
+  const [isRunningDown, setRunningDown] = useState(false);
+
+    const countEl = useRef(null);
+    const counterTemp = () => {
+        if (countEl.current >= 15) {
+            setTempColor = 'hot';
+        } else if(countEl < 15) {
+            setTempColor = 'cold';
+        }
+
+    };
+
+  useEffect(() => {
+      if(isRunning) {
+          const id = window.setInterval(() => {
+              setValue(value => value + 1);
+          }, 125);
+          return () => window.clearInterval(id);
+      }
+      if(isRunningDown) {
+          const id = window.setInterval(() => {
+              setValue(value => value - 1);
+          }, 125);
+          return () => window.clearInterval(id);
+      }
+  }, [isRunning, isRunningDown]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='app-container'>
+        <div className='temperature-display-container'>
+          <div className={`temperature-display ${tempColor}`}>{value}°C</div>
+        </div>
+        <div className='button-container'>
+          <button ref={counterTemp()} onMouseDown={()=> setRunningDown(true)} onMouseUp={()=> setRunningDown(false)}>-</button>
+          <button onMouseDown={()=> setIsRunning(true)} onMouseUp={()=> setIsRunning(false)}>+</button>
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
